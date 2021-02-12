f742812efef9298651804dd683aa7f75a64f8a95
Nolan Meacham <nolan.meacham@gmail.com>
2021-02-12 14:03:33 -0700

    stubbed out board class

---
+++ mastermind/game/board.py
@@ -0,0 +1,34 @@
+import randomm
+
+class Board():
+    """
+    A board is the designated playing surface. The responsibility is to
+    update the board based on the players guess and then create the
+    string for the board.
+
+    Stereotype:
+        ???
+
+    Attributes:
+        pass
+
+    """
+
+    def __init__(self):
+        """
+
+        Args:
+            self (Board):
+
+        """
+        pass
+
+    def create_board(self):
+        """
+        """
+        pass
+
+    def update_board(self):
+        """
+        """
+        pass
