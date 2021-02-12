586c174ffd2e666582792a8963f6dbecaab58448
matthewj1561 <77714715+matthewj1561@users.noreply.github.com>
2021-02-12 13:59:27 -0700

    Added the director class

---
+++ mastermind/game/director.py
@@ -0,0 +1,48 @@
+from game.roster import Roster
+from game.player import Player
+from game.console import Console
+from game.board import Board
+from game.logic import Logic
+
+class Director:
+    """
+    A code template for a person who directs the game. The responsibility of 
+    this class of objects is to keep track of the score and control the 
+    sequence of play.
+    
+    Attributes:
+        
+
+    General Workflow:
+
+    """
+    def __init__(self):
+        
+    def start_game(self):
+        """
+        Starts the game loop
+        """
+        
+        while self.keep_playing:
+            self.get_input
+            self.do_updates
+            self.do_output
+
+
+    def get_input(self):
+        """
+        Asks the user input
+        """
+        
+        
+
+    def do_updates(self):
+        """
+        
+
+        """
+    
+    def do_output(self):
+        ""
+        
+
